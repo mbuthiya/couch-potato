537416a32921cbd2909e2ba23055f4b2abf54de2
James Muriuki <jamesmuriukimbuthia@gmail.com>
2017-07-31 17:35:31 +0300

    Create html files for most routes

--- app/views.py
+++ app/views.py
@@ -27,3 +27,53 @@ def users(name):
     '''
     title = f" The couch potato= profile {name}"
     return render_template('users.html', name = name,title=title)
+
+# Movies routes
+@app.route('/movies')
+def movies():
+    '''
+    Movies Route function to render movies.
+    Returns:
+        Returns the movies html page and passes in the dynamic name to it.
+    '''
+    title = "The couch potato - Movies"
+    return render_template('movies.html',title = title)
+
+
+# Tv Shows
+@app.route('/shows')
+def shows():
+    '''
+    Shows view  function  to display tv shows
+    Returns:
+        Returns the Tv shows html page.
+    '''
+    title = "The couch potato - Tv-Shows"
+    return render_template('shows.html',title = title)
+
+
+# Discover
+@app.route('/discover')
+def discover():
+    '''
+    Discover view  function  to display Random tv shows
+
+    '''
+    title = "The couch potato - Discover"
+    return render_template('discover.html',title = title)
+
+
+# Login
+@app.route('/login')
+def login():
+
+    title = "The couch potato - Log In"
+    return render_template('login.html',title = title)
+
+
+# sign up
+@app.route('/signup')
+def signup():
+    
+    title = "The couch potato - Sign Up"
+    return render_template('signup.html',title = title)
