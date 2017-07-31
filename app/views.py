6f145b81a8f9574bde9556394a20d91da7d2c15e
James Muriuki <jamesmuriukimbuthia@gmail.com>
2017-07-31 15:11:52 +0300

    Add Proper application structure and set up for deployment
    Created a basic application structure and set up for heroku deployment

--- app/views.py
+++ app/views.py
@@ -1,7 +1,5 @@
-from flask import Flask,render_template
-app = Flask(__name__)
-
-
+from  flask import render_template
+from app import app
 # Index route
 @app.route('/')
 @app.route('/index')
@@ -27,9 +25,3 @@ def users(name):
         Returns the users html page and passes in the dynamic name to it.
     '''
     return render_template('users.html', name = name)
-
-
-
-
-if __name__ == '__main__':
-    app.run(debug = True)
---
+++ run.py
@@ -0,0 +1,5 @@
+#!virtual/bin/python3.6
+from app import app
+
+if __name__ == '__main__':
+    app.run(debug = True)
